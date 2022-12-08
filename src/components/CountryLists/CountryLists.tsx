e2984defdc524e605a4efe7add6e25b07a3b7ffc
hutamatr <hutamatr@gmail.com>
2022-12-09 02:27:49 +0700

    create pagination & use in countries list

--- src/components/CountryLists/CountryLists.tsx
+++ src/components/CountryLists/CountryLists.tsx
@@ -1,26 +1,57 @@
-import { useQuery } from '@tanstack/react-query';
+import { AxiosResponse, AxiosError } from 'axios';
+import { Country } from '../../hooks/useHttp';
+import Card from '../UI/Card';
 
-import FilterBy from '../FilterBy/FilterBy';
-import useHttp from '../../hooks/useHttp';
+export interface State {
+  isLoading: boolean;
+  isError: boolean;
+  error: unknown;
+}
 
-const CountryLists = () => {
-  const { requestHttp } = useHttp();
+interface Props extends State {
+  onPaginationData: () => Country[] | undefined;
+}
 
-  const {
-    data: countriesData,
-    isLoading,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: ['countries'],
-    queryFn: () => requestHttp({ method: 'GET', url: 'all' }),
-  });
+const CountryLists = ({
+  onPaginationData,
+  isLoading,
+  isError,
+  error,
+}: Props) => {
+  const countryContent = (
+    <div className="grid grid-flow-row grid-cols-1 items-center justify-items-center gap-6 sm:grid-cols-2 sm:gap-8 md:grid-cols-3 lg:grid-cols-4 lg:gap-12">
+      {onPaginationData()?.map((country, index) => {
+        const populationFormat = new Intl.NumberFormat().format(
+          country.population
+        );
 
-  console.log(countriesData);
+        return (
+          <Card
+            countryImage={country.flags.png}
+            country={country.name.common}
+            population={populationFormat}
+            region={country.region}
+            capital={country.capital}
+            key={index}
+          />
+        );
+      })}
+    </div>
+  );
 
   return (
-    <section>
-      <FilterBy />
+    <section className="flex min-h-screen flex-col gap-y-4">
+      {isLoading && (
+        <p className="text-center text-2xl font-semibold text-very-dark-blue-2 dark:text-white">
+          Loading...
+        </p>
+      )}
+      {isError && error instanceof AxiosError && (
+        <p className="text-center text-2xl font-semibold text-red-600">
+          {error.response?.statusText}
+        </p>
+      )}
+      {!isLoading && !isError && countryContent}
     </section>
   );
 };
