f4ab5181945ffb3b2009d35c6d8ac6e5cd3e63b3
hutamatr <hutamatr@gmail.com>
2022-12-12 08:47:00 +0700

    create back-button component

---
+++ src/components/UI/BackButton.tsx
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
+import { MdKeyboardBackspace } from 'react-icons/md';
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      className="flex w-fit flex-row items-center justify-center gap-x-3 rounded py-1 px-3 font-medium shadow-material dark:bg-dark-blue dark:text-white"
+      onClick={() => navigate('/', { replace: true })}
+    >
+      <MdKeyboardBackspace className="text-lg" />
+      Back
+    </button>
+  );
+};
+
+export default BackButton;
